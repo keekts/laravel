d6494a37920de14b3048302935d58166aea6a20c
Chanthavong <channounta@gmail.com>
2021-07-18 11:06:19 +0700

    add dwl db and fix docker compose contianer name

--- db/bookstore-dev.sql
+++ db/bookstore-dev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Jun 27, 2021 at 07:40 AM
+-- Generation Time: Jul 18, 2021 at 04:05 AM
 -- Server version: 10.4.19-MariaDB-1:10.4.19+maria~focal
 -- PHP Version: 7.4.20
 
@@ -52,13 +52,13 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `code`, `name`, `description`, `tag`, `star`, `stock`, `cover`, `image`, `price`, `price_cost`, `price_discount`, `type_id`, `isbn`, `author`, `detail`, `qty`) VALUES
-(6, '', 'One Last Stop Casey ', '', '', NULL, NULL, 'uploads/2021/06/26/TMGz46S2EYxBsKRlrPFA9XIUCfH1uQ.jpg', 'uploads/2021/06/26/TMGz46S2EYxBsKRlrPFA9XIUCfH1uQ.jpg', '10000.00', '0.00', NULL, 1, NULL, NULL, NULL, 1),
-(7, '', 'Glory Days: The Summ', '<p><br></p><p><a href=\"https://www.amazon.com/dp/1328637247/ref=s9_acsd_hps_bw_c2_x_3_t?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-8&amp;pf_rd_r=54VNGJ1BGPS7F3ETR9B8&amp;pf_rd_t=101&amp;pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&amp;pf_rd_i=2', '', NULL, NULL, 'uploads/2021/06/26/VRyQrNG5zgvat3Uh90PpoAnj4iMH6m.jpg', 'uploads/2021/06/26/VRyQrNG5zgvat3Uh90PpoAnj4iMH6m.jpg', '10000.00', '10.00', NULL, 2, NULL, NULL, NULL, 3),
-(41, '', 'Haven Point: A Novel', '', '', NULL, NULL, 'uploads/2021/06/26/RpVsozrO1MGxeDv9B7I2YWfEXJAayP.jpg', 'uploads/2021/06/26/RpVsozrO1MGxeDv9B7I2YWfEXJAayP.jpg', '100000.00', '0.00', NULL, 2, NULL, NULL, NULL, 0),
-(42, '', 'Preventable: The Ins', '<p><a href=\"https://www.amazon.com/dp/1250770165/ref=s9_acsd_hps_bw_c2_x_1_t?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-8&amp;pf_rd_r=54VNGJ1BGPS7F3ETR9B8&amp;pf_rd_t=101&amp;pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&amp;pf_rd_i=283155\" rel=', '', NULL, NULL, 'uploads/2021/06/26/hSH1Iwiq32QYUPc9mLMaN4JvOdACtK.jpg', 'uploads/2021/06/26/hSH1Iwiq32QYUPc9mLMaN4JvOdACtK.jpg', '100000.00', NULL, NULL, 2, NULL, NULL, NULL, 5),
-(43, '', 'Ridgeline: A Novel M', '<p><a href=\"https://www.amazon.com/dp/1250310466/ref=s9_acsd_hps_bw_c2_x_4_t?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-8&amp;pf_rd_r=54VNGJ1BGPS7F3ETR9B8&amp;pf_rd_t=101&amp;pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&amp;pf_rd_i=283155\" rel=', '', NULL, NULL, 'uploads/2021/06/26/PzMW0fE3aYlp2xsXdrqDbjAU8iR6Ov.jpg', 'uploads/2021/06/26/PzMW0fE3aYlp2xsXdrqDbjAU8iR6Ov.jpg', '80000.00', '0.00', NULL, 2, NULL, NULL, NULL, 2),
+(6, '', 'One Last Stop Casey ', '', '', NULL, NULL, 'uploads/2021/06/26/TMGz46S2EYxBsKRlrPFA9XIUCfH1uQ.jpg', 'uploads/2021/06/26/TMGz46S2EYxBsKRlrPFA9XIUCfH1uQ.jpg', '10000.00', '0.00', NULL, 1, NULL, NULL, NULL, -1),
+(7, '', 'Glory Days: The Summ', '<p><br></p><p><a href=\"https://www.amazon.com/dp/1328637247/ref=s9_acsd_hps_bw_c2_x_3_t?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-8&amp;pf_rd_r=54VNGJ1BGPS7F3ETR9B8&amp;pf_rd_t=101&amp;pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&amp;pf_rd_i=2', '', NULL, NULL, 'uploads/2021/06/26/VRyQrNG5zgvat3Uh90PpoAnj4iMH6m.jpg', 'uploads/2021/06/26/VRyQrNG5zgvat3Uh90PpoAnj4iMH6m.jpg', '10000.00', '10.00', NULL, 2, NULL, NULL, NULL, -1),
+(41, '', 'Haven Point: A Novel', '', '', NULL, NULL, 'uploads/2021/06/26/RpVsozrO1MGxeDv9B7I2YWfEXJAayP.jpg', 'uploads/2021/06/26/RpVsozrO1MGxeDv9B7I2YWfEXJAayP.jpg', '100000.00', '20000.00', NULL, 2, NULL, NULL, NULL, 0),
+(42, '', 'Preventable: The Ins', '<p><a href=\"https://www.amazon.com/dp/1250770165/ref=s9_acsd_hps_bw_c2_x_1_t?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-8&amp;pf_rd_r=54VNGJ1BGPS7F3ETR9B8&amp;pf_rd_t=101&amp;pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&amp;pf_rd_i=283155\" rel=', '', NULL, NULL, 'uploads/2021/06/26/hSH1Iwiq32QYUPc9mLMaN4JvOdACtK.jpg', 'uploads/2021/06/26/hSH1Iwiq32QYUPc9mLMaN4JvOdACtK.jpg', '100000.00', NULL, NULL, 2, NULL, NULL, NULL, 13),
+(43, '', 'Ridgeline: A Novel M', '<p><a href=\"https://www.amazon.com/dp/1250310466/ref=s9_acsd_hps_bw_c2_x_4_t?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-8&amp;pf_rd_r=54VNGJ1BGPS7F3ETR9B8&amp;pf_rd_t=101&amp;pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&amp;pf_rd_i=283155\" rel=', '', NULL, NULL, 'uploads/2021/06/26/PzMW0fE3aYlp2xsXdrqDbjAU8iR6Ov.jpg', 'uploads/2021/06/26/PzMW0fE3aYlp2xsXdrqDbjAU8iR6Ov.jpg', '80000.00', '0.00', NULL, 2, NULL, NULL, NULL, -1),
 (45, '', 'The Burning Blue: Th', '<p><a href=\"https://www.amazon.com/dp/1250755557/ref=s9_acsd_hps_bw_c2_x_5_t?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-8&amp;pf_rd_r=54VNGJ1BGPS7F3ETR9B8&amp;pf_rd_t=101&amp;pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&amp;pf_rd_i=283155\" rel=', '', NULL, NULL, 'uploads/2021/06/26/4D3JBlPeghiaNd0Qk9KC8E1tnu2jZr.jpg', 'uploads/2021/06/26/4D3JBlPeghiaNd0Qk9KC8E1tnu2jZr.jpg', '50000.00', NULL, NULL, 9, NULL, NULL, NULL, 2),
-(46, '', 'The Extended Mind: T', '<p><a href=\"https://www.amazon.com/dp/0544947665/ref=s9_acsd_hps_bw_c2_x_6_t?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-8&amp;pf_rd_r=54VNGJ1BGPS7F3ETR9B8&amp;pf_rd_t=101&amp;pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&amp;pf_rd_i=283155\" rel=', '', NULL, NULL, 'uploads/2021/06/26/QCc7WfIAGSFYJdkEu4NbwDRn1stTqK.jpg', 'uploads/2021/06/26/QCc7WfIAGSFYJdkEu4NbwDRn1stTqK.jpg', NULL, NULL, NULL, 10, NULL, NULL, NULL, -1);
+(46, '', 'The Extended Mind: T', '<p><a href=\"https://www.amazon.com/dp/0544947665/ref=s9_acsd_hps_bw_c2_x_6_t?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-8&amp;pf_rd_r=54VNGJ1BGPS7F3ETR9B8&amp;pf_rd_t=101&amp;pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&amp;pf_rd_i=283155\" rel=', '', NULL, NULL, 'uploads/2021/06/26/QCc7WfIAGSFYJdkEu4NbwDRn1stTqK.jpg', 'uploads/2021/06/26/QCc7WfIAGSFYJdkEu4NbwDRn1stTqK.jpg', NULL, NULL, NULL, 10, NULL, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -93,6 +93,9 @@ CREATE TABLE `customers` (
   `first` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
   `last` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
   `phone` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` varchar(20) COLLATE utf8_unicode_ci DEFAULT 'Pending',
+  `email` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -100,10 +103,11 @@ CREATE TABLE `customers` (
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`id`, `first`, `last`, `phone`, `created_at`) VALUES
-(2, 'daimon', 'xxx', '2223 5544', '2021-06-24 04:22:27'),
-(3, 'Poukky', 'test', NULL, '2021-06-24 04:23:47'),
-(4, 'customer', NULL, NULL, '2021-06-27 05:20:19');
+INSERT INTO `customers` (`id`, `first`, `last`, `phone`, `status`, `email`, `password`, `created_at`) VALUES
+(2, 'daimon', 'xxx', '2223 5544', 'Approved', NULL, NULL, '2021-06-24 04:22:27'),
+(3, 'Poukky', 'test', NULL, 'Blocked', NULL, NULL, '2021-06-24 04:23:47'),
+(4, 'customer', NULL, NULL, 'Approved', NULL, NULL, '2021-06-27 05:20:19'),
+(5, 'test', 'test', '333443 e444', 'Pending', 'test@gmail.com', '$2y$10$vq2SxToiIsBpAXatln/4Yuy6.z/CeGRSW4xoBLNd1gePAA5jufAFO', '2021-07-11 07:29:59');
 
 -- --------------------------------------------------------
 
@@ -132,7 +136,7 @@ CREATE TABLE `employees` (
 --
 
 INSERT INTO `employees` (`id`, `first`, `last`, `phone`, `gender`, `age`, `address`, `photo`, `auther`, `email`, `username`, `password`, `created_at`) VALUES
-(1, 'daimon', NULL, '7787 4433', 'F', NULL, NULL, 'uploads/profile/BpVJqLKTgD.jpeg', 'Admin', 'daimon@gmail.com', 'admin', '$2y$10$il2tvy9ykMg85S99FOHSV.3wV1xzOMrphuggnXClfXqe.KLXd4lXO', '2021-06-23 06:34:28'),
+(1, 'daimon', 'd', '7787 4433', 'F', 19, NULL, 'uploads/profile/NjxSe4O27Z.png', 'Admin', 'daimon@gmail.com', 'admin', '$2y$10$il2tvy9ykMg85S99FOHSV.3wV1xzOMrphuggnXClfXqe.KLXd4lXO', '2021-06-23 06:34:28'),
 (2, 'user', 'lasrt', '4443 5566', 'F', 12, 'address', 'uploads/profile/fnDumYMT3W.jpg', 'User', 'user@gmail.com', 'user', '$2y$10$GKi03E97/TXqcgbT4bDhSO77SGeolrQRCfTeugrDOmCDjhAOw/MXq', '2021-06-23 06:39:41');
 
 -- --------------------------------------------------------
@@ -159,7 +163,13 @@ INSERT INTO `imports` (`id`, `import_date`, `price`, `qty`, `purchaser_detail_id
 (26, '2021-06-26 00:00:00', '0.00', 1, 9),
 (27, '2021-06-26 00:00:00', '0.00', 1, 8),
 (28, '2021-06-26 00:00:00', '0.00', 1, 7),
-(29, '2021-06-26 00:00:00', '0.00', 3, 3);
+(29, '2021-06-26 00:00:00', '0.00', 3, 3),
+(30, '2021-07-10 00:00:00', '0.00', 1, 9),
+(31, '2021-07-10 00:00:00', '0.00', 1, 9),
+(32, '2021-07-10 00:00:00', '0.00', 3, 3),
+(33, '2021-07-10 00:00:00', '0.00', 3, 3),
+(34, '2021-07-10 00:00:00', '1300.00', 3, 3),
+(35, '2021-07-10 00:00:00', '20000.00', 1, 7);
 
 -- --------------------------------------------------------
 
@@ -183,8 +193,9 @@ INSERT INTO `purchaser_orders` (`id`, `purchaser_date`, `supplier_id`, `emp_id`)
 (2, '2021-06-24 00:00:00', 1, 1),
 (3, '2021-06-24 00:00:00', 1, 1),
 (4, '2021-06-24 00:00:00', 3, 1),
-(5, '2021-06-24 00:00:00', 1, 1),
-(6, '2021-06-24 00:00:00', 1, 1);
+(5, '2021-05-07 00:00:00', 1, 1),
+(6, '2021-06-24 00:00:00', 1, 1),
+(7, '2021-07-11 00:00:00', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -212,7 +223,9 @@ INSERT INTO `purchaser_order_details` (`id`, `purchaser_order_id`, `qty`, `book_
 (8, 6, 1, 43, 1),
 (9, 6, 1, 46, 1),
 (11, 4, 2, 7, 0),
-(12, 4, 2, 43, 0);
+(12, 4, 2, 43, 0),
+(13, 7, 1, 42, 0),
+(14, 7, 2, 45, 0);
 
 -- --------------------------------------------------------
 
@@ -241,7 +254,12 @@ INSERT INTO `sells` (`id`, `status`, `status_payment`, `customer_id`, `emp_id`, 
 (4, 'sell', 'payed', 3, 1, '2021-05-10 00:00:00', '2021-06-26 11:08:29'),
 (5, 'sell', 'payed', 2, 1, '2021-06-26 18:35:47', '2021-06-26 11:35:47'),
 (6, 'order', 'payed', 3, 1, '2021-04-13 18:36:15', '2021-06-26 11:36:15'),
-(7, 'sell', 'payed', 3, 1, '2021-06-27 12:19:31', '2021-06-27 05:19:31');
+(7, 'sell', 'payed', 3, 1, '2021-06-27 12:19:31', '2021-06-27 05:19:31'),
+(8, 'sell', 'payed', 4, 1, '2021-07-10 16:50:24', '2021-07-10 09:50:24'),
+(9, 'sell', 'payed', 3, 1, '2021-07-10 17:31:23', '2021-07-10 10:31:24'),
+(10, 'sell', 'payed', 3, 1, '2021-07-11 11:31:30', '2021-07-11 04:31:30'),
+(17, 'sell', 'payed', 5, 1, '2021-07-11 09:53:54', '2021-07-11 09:53:54'),
+(18, 'sell', 'payed', 5, 1, '2021-07-11 11:24:09', '2021-07-11 11:24:09');
 
 -- --------------------------------------------------------
 
@@ -271,7 +289,22 @@ INSERT INTO `sell_details` (`id`, `qty`, `price`, `sell_id`, `book_id`) VALUES
 (16, 1, '100000.00', 5, 42),
 (17, 2, '50000.00', 5, 45),
 (18, 1, '100000.00', 7, 42),
-(19, 3, NULL, 7, 46);
+(21, 1, '100000.00', 6, 41),
+(24, 1, '10000.00', 8, 7),
+(25, 1, '50000.00', 8, 45),
+(26, 1, '100000.00', 8, 42),
+(27, 1, '10000.00', 9, 7),
+(28, 1, '80000.00', 9, 43),
+(29, 1, '50000.00', 9, 45),
+(30, 2, '80000.00', 10, 43),
+(31, 1, '50000.00', 10, 45),
+(48, 1, '10000.00', 17, 6),
+(49, 1, '10000.00', 17, 7),
+(50, 1, '80000.00', 17, 43),
+(51, 1, '50000.00', 17, 45),
+(52, 2, '10000.00', 18, 6),
+(53, 2, '10000.00', 18, 7),
+(54, 1, '100000.00', 18, 41);
 
 -- --------------------------------------------------------
 
@@ -389,7 +422,7 @@ ALTER TABLE `book_types`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `employees`
@@ -401,31 +434,31 @@ ALTER TABLE `employees`
 -- AUTO_INCREMENT for table `imports`
 --
 ALTER TABLE `imports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `purchaser_orders`
 --
 ALTER TABLE `purchaser_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `purchaser_order_details`
 --
 ALTER TABLE `purchaser_order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `sells`
 --
 ALTER TABLE `sells`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `sell_details`
 --
 ALTER TABLE `sell_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
